f2142f4f0e1bbf849316936cbd8ba6b9e020a7ab
Tanmay Jain <tanmayjain4477@gmail.com>
2024-12-07 22:48:54 +0530

    Added production db

--- db/config.py
+++ db/config.py
@@ -1,28 +1,12 @@
-# from sqlmodel import SQLModel, create_engine, Session
-# from models import *
-
-# # Update the database URL for SQLite
-# sqlite_db_url = 'sqlite:///sqlite.db'  # Using SQLite database file named 'sqlite.db'
-# engine = create_engine(sqlite_db_url, echo=True)
-
-# def start_db():
-#     print("Server is running with SQLite instance")
-#     SQLModel.metadata.create_all(engine)
-
-# def get_session():
-#     with Session(engine) as session:
-#         print("Session started")
-#         yield session
-
-
 from sqlmodel import SQLModel, create_engine, Session
 from models import *
 
-postgres_db_url = 'postgresql+psycopg://db_username:db_password@localhost:5432/mydbmeta'
+# local_postgres_db_url = 'postgresql+psycopg://db_username:db_password@localhost:5432/mydbmeta'
+postgres_db_url = 'postgresql+psycopg://postgres:mysecretpassword@3.110.48.240:5432/postgres'
 engine = create_engine(postgres_db_url, echo=True)
 
 def start_db():
-    print("Server is running with SQLite instance")
+    print("Server is running with Postgres instance")
     SQLModel.metadata.create_all(engine)
 
 def get_session():
